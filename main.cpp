c8178359e7ad7fe59ad4cb75ac9c3039b38dc617
Abdisis Abrahim <aaaczv@umsystem.edu>
2021-04-11 11:04:07 -0500

    Added the while loop and function that will check the delimiters.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,51 @@
 #include "DelimiterStack.h"
 using namespace std;
 
+void CheckDelim(string, DelimiterStack&, int);
+
 int main() {
     char delimiter;
-    int line;
+    int line =0;
     int counter;
+    string input;
     DelimiterStack stack;
-
+    do {
+        cout << "" << endl;
+        getline(cin, input);
+        line++;
+        CheckDelim(input, stack, line);
+    } while (input != "DONE");
 
 
 
     return 0;
+}
+void CheckDelim(string input, DelimiterStack& stack, int line ){
+    char c;
+    int num;
+    int count;
+    for (int i = 0; i < input.length(); ++i) {
+        switch (input[i]) {
+            case '(':
+            case '[':
+            case '{':
+                stack.push(input[i], line, i+1);
+                break;
+            case ')':
+            case ']':
+            case '}':
+                stack.pop(c, num, count);
+                if (c != '{'){
+                    cout << "Mismatched delimiter " << c << " found at line " << num <<  ", char " << count << " does not match } found at line " << line << ", char " << i +1 << endl;
+                }
+                break;
+
+
+            default:
+                break;
+        }
+
+    }
+
+
 }
