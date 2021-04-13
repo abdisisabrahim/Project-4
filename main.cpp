1557ebcf7588d3421f00ee89dbd71c026890eb5e
Abdisis Abrahim <aaaczv@umsystem.edu>
2021-04-13 10:26:11 -0500

    Finished CheckDelim function.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void CheckDelim(string, DelimiterStack&, int);
 
+
 int main() {
     char delimiter;
     int line =0;
@@ -33,11 +34,39 @@ void CheckDelim(string input, DelimiterStack& stack, int line ){
                 stack.push(input[i], line, i+1);
                 break;
             case ')':
+                if (stack.isEmpty()){
+                    cout << "Right Delimiter ) had no left delimiter found at line " << line << ", char " << i +1 << endl;
+                }
+                else{
+
+                    stack.pop(c, num, count);
+                    if (c != '('){
+                        cout << "Mismatched delimiter " << c << " found at line " << num <<  ", char " << count << " does not match ) found at line " << line << ", char " << i +1 << endl;
+                    }
+                }
+                break;
             case ']':
+                if (stack.isEmpty()){
+                    cout << "Right Delimiter ] had no left delimiter found at line " << line << ", char " << i +1 << endl;
+                }
+                else{
+
+                    stack.pop(c, num, count);
+                    if (c != '['){
+                        cout << "Mismatched delimiter " << c << " found at line " << num <<  ", char " << count << " does not match ] found at line " << line << ", char " << i +1 << endl;
+                    }
+                }
+                break;
             case '}':
-                stack.pop(c, num, count);
-                if (c != '{'){
-                    cout << "Mismatched delimiter " << c << " found at line " << num <<  ", char " << count << " does not match } found at line " << line << ", char " << i +1 << endl;
+                if (stack.isEmpty()){
+                    cout << "Right Delimiter } had no left delimiter found at line " << line << ", char " << i +1 << endl;
+                }
+                else{
+
+                    stack.pop(c, num, count);
+                    if (c != '{'){
+                        cout << "Mismatched delimiter " << c << " found at line " << num <<  ", char " << count << " does not match } found at line " << line << ", char " << i +1 << endl;
+                    }
                 }
                 break;
 
